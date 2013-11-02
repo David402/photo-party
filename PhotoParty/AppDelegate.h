663ce0205843447a1f91cd7347b4f876a79c034e
David Liu <shaowei.liu@gmail.com>
2013-11-02 16:49:16 +0800

    init

---
+++ PhotoParty/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhotoParty
+//
+//  Created by David Liu on 11/2/13.
+//  Copyright (c) 2013 David Lliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhotoPartyTests/PhotoPartyTests.m
@@ -0,0 +1,34 @@
+//
+//  PhotoPartyTests.m
+//  PhotoPartyTests
+//
+//  Created by David Liu on 11/2/13.
+//  Copyright (c) 2013 David Lliu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PhotoPartyTests : XCTestCase
+
+@end
+
+@implementation PhotoPartyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
